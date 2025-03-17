ac9ce9b0c629d6c56beec9405182a16defae9d2d
Deepak7781 <deepak21082005@gmail.com>
2025-03-17 21:41:03 +0530

    initial commit

---
+++ main.m
@@ -0,0 +1,15 @@
+% Blade Element Theory
+
+
+% Physical Parameters
+
+c = 19; % chord length
+R = 10; % Radius
+R_h = 12; % Hub Radius
+beta = deg2rad(5); % geometric pitch angle
+B = 3; % Number of blades
+rho = 1.225; % Air density at sea level
+V = 20; % forward velocity
+omega = 10; % Angular velocity 
+
+
