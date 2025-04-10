50bd591761d43d594d42e4e30190c201540a5eff
Deepak7781 <deepak21082005@gmail.com>
2025-04-10 18:08:11 +0530

    Cl and Cd using interpolation

--- main.m
+++ main.m
@@ -13,16 +13,17 @@ omega = 300 * 2*pi/60; % Angular velocity
 N = 20;
 
 
-% Airfoil data
-a0 = 5.7;
-alpha0 = 0;
-Cd0 = 0.01;
-k = 0.01;
+% load airfoil data
+data = load('polar_1232.txt');
+
+alpha_table = data(:,1);
+Cl_table = data(:,2);
+Cd_table = data(:,3);
 
 % Discretization
 
 r = linspace(0.1*R,R,N);
-dr = r(2)-r(1);
+dr = r(2)-r(1); 
 
 T = 0; Q = 0;
 
@@ -34,9 +35,10 @@ for i = 1:N
 
     phi = atan2(V_a,V_t);
     alpha = phi - theta;
+    alpha_deg = rad2deg(alpha);
 
-    Cl = a0 * alpha;
-    Cd = Cd0 + (k*(Cl^2));
+    Cl = interp1(alpha_table, Cl_table, alpha_deg, 'linear', 'extrap');
+    Cd = interp1(alpha_table, Cd_table, alpha_deg, 'linear', 'extrap');
 
     dL = 0.5 * rho * (V_res^2)* c * Cl * dr;
     dD = 0.5 * rho * (V_res^2) * c * Cd * dr;
